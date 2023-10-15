14b77ed0ca8695d13e52c30a5b8e5b534da5fd13
Jagdamba Tripathi <jagdtri2003@gmail.com>
2023-10-15 23:42:02 +0530

    added Porgram 5

---
+++ WT/Prog5.java
@@ -0,0 +1,18 @@
+package WT;
+
+class Demo{
+    Demo(){
+        System.out.println("Default Constructor !!");
+    }
+    Demo(int a){
+        this();
+        System.out.println("Parametrized Constructor !!");
+    }
+}
+
+public class Prog5 {
+    public static void main(String[] args) {
+        Demo aObj = new Demo(10);
+    }
+
+}
